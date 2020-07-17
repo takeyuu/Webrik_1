0671da71a9275f024ae6f684075b4c3247880f76
takeoka <tokih4ru@gmail.com>
2020-07-17 15:21:48 +0900

    update

--- goya.rb
+++ goya.rb
@@ -4,12 +4,30 @@ cgi = CGI.new
 # データを受け取った後、HTMLの形式でレスポンスを返す
 cgi.out("type" => "text/html", "charset" => "UTF-8") {
   # 情報の目印とした"goya"のデータを cgi['goya']と言う記述で取り出し、ローカル変数に代入する
-  get = cgi['goya']
-  # HTMLでレスポンスを返却する
-  "<html>
-    <body>
-      <p>ゴーヤの大きさと売った相手の情報は下記になります</p>
-      文字列：#{get}
-    </body>
-  </html>"
+  if cgi.has_key?('goya')
+    get = cgi['goya']
+    # HTMLでレスポンスを返却する
+    "<html>
+      <body>
+        <p>ゴーヤの大きさと売った相手の情報は下記になります</p>
+        文字列：#{get}
+      </body>
+    </html>"
+  elsif cgi.has_key?('goya_not_self')
+    get_goya_not_self = cgi['goya_not_self']
+    "<html>
+      <body>
+        <p>譲渡先が自家消費以外のゴーヤ情報は下記になります<p>
+       #{get_goya_not_self}
+      </body>
+    </html>"
+  elsif cgi.has_key?('goya_false')
+    get_goya_false = cgi['goya_false']
+    "<html>
+      <body>
+        <p>品質が悪いゴーヤの情報は以下になります</p>
+        #{get_goya_false}
+      </body>
+    </html>"
+  end
 }
